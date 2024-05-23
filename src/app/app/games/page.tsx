0ba5ec9300972c949c07b06e3d8c9b9c5733bada
Michael Wickett <mwickett@hashicorp.com>
2024-05-23 14:14:31 -0400

    scaffold basics

---
+++ src/app/app/games/page.tsx
@@ -0,0 +1,7 @@
+export default function Games() {
+  return (
+    <div>
+      <h2>This is the games page</h2>
+    </div>
+  );
+}
---
+++ src/app/app/stats/page.tsx
@@ -0,0 +1,7 @@
+export default function Stats() {
+  return (
+    <div>
+      <h2>This is the stats page</h2>
+    </div>
+  );
+}
