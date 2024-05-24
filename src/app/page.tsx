6e57ffdbd8853cec9259cbab1cf606d2d9e7cc82
Michael Wickett <mwickett@hashicorp.com>
2024-05-24 16:39:44 -0400

    Add link from homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>This is the homepage</h1>
+      <Link href="/app">Go to Dashboard</Link>
     </main>
   );
 }
