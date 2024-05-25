d09b0938f04b178769c0ef06ecc07b493e064046
Michael Wickett <mwickett@hashicorp.com>
2024-05-25 11:22:39 -0400

    start querying in earnest

--- src/app/app/games/page.tsx
+++ src/app/app/games/page.tsx
@@ -1,16 +1,37 @@
 import prisma from "../../../../db";
 import { auth, currentUser } from "@clerk/nextjs/server";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 export default async function Games() {
   const { userId } = auth();
   let games;
+  // TODO bail and error if no user found=
   if (userId) {
-    games = await prisma.user.findMany({
+    games = await prisma.game.findMany({
       where: {
-        clerk_user_id: userId,
+        players: {
+          some: {
+            user: {
+              clerk_user_id: userId,
+            },
+          },
+        },
       },
       include: {
-        games: true,
+        players: {
+          include: {
+            user: true,
+          },
+        },
+        scores: true,
       },
     });
     console.log(games);
@@ -20,6 +41,19 @@ export default async function Games() {
     <div>
       <h2>This is the games page</h2>
       <pre>{JSON.stringify(games, null, 2)}</pre>
+
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>ID</TableHead>
+            <TableHead>Date</TableHead>
+            <TableHead>Status</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>This is a row</TableRow>
+        </TableBody>
+      </Table>
     </div>
   );
 }
