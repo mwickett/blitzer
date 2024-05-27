61d0b73d604dc96a8ff1bb1cbe2f82799e5ea0a8
Michael Wickett <mwickett@hashicorp.com>
2024-05-27 14:13:02 -0400

    hackin, shifting to RESTful API

--- src/app/app/games/page.tsx
+++ src/app/app/games/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import prisma from "../../../../db";
 import { auth, currentUser } from "@clerk/nextjs/server";
 import {
@@ -38,22 +39,45 @@ export default async function Games() {
     console.log(userId);
   }
   return (
-    <div>
+    <section>
       <h2>This is the games page</h2>
-      <pre>{JSON.stringify(games, null, 2)}</pre>
+      <Suspense fallback={<div>Loading...</div>}>
+        <pre>{JSON.stringify(games, null, 2)}</pre>
 
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead>ID</TableHead>
-            <TableHead>Date</TableHead>
-            <TableHead>Status</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          <TableRow>This is a row</TableRow>
-        </TableBody>
-      </Table>
-    </div>
+        <Table>
+          <TableCaption>A list of games</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[100px]">ID</TableHead>
+              <TableHead>Date</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead>Players</TableHead>
+              <TableHead className="text-right">Winner</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {games &&
+              games.map((game) => (
+                <TableRow key={game.id}>
+                  <TableCell>{game.id}</TableCell>
+                  <TableCell>{game.createdAt.toString()}</TableCell>
+                  <TableCell>
+                    {game.isFinished ? "Complete" : "In Progress"}
+                  </TableCell>
+                  <TableCell>
+                    {game.players.map((player) => player.user.email).join(", ")}
+                  </TableCell>
+                  <TableCell className="text-right">
+                    {game.winner ? game.winner.email : "None"}{" "}
+                  </TableCell>
+                </TableRow>
+              ))}
+            <TableRow>
+              <TableCell></TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </Suspense>
+    </section>
   );
 }
