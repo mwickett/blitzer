003ad4c4eb9d059c8d869dbf2f3f0160c2bce8d4
Michael Wickett <mwickett@hashicorp.com>
2024-05-28 17:43:34 -0400

    making some progress

--- src/app/app/games/page.tsx
+++ src/app/app/games/page.tsx
@@ -1,83 +1,88 @@
-import { Suspense } from "react";
-import prisma from "../../../../db";
-import { auth, currentUser } from "@clerk/nextjs/server";
+// components/GamesList.server.tsx
+import { auth } from "@clerk/nextjs/server";
+import prisma from "@/db";
 import {
   Table,
   TableBody,
-  TableCaption,
   TableCell,
   TableHead,
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
-export default async function Games() {
+export default async function GamesList() {
   const { userId } = auth();
-  let games;
-  // TODO bail and error if no user found=
-  if (userId) {
-    games = await prisma.game.findMany({
-      where: {
-        players: {
-          some: {
-            user: {
-              clerk_user_id: userId,
-            },
-          },
-        },
-      },
-      include: {
-        players: {
-          include: {
-            user: true,
-          },
-        },
-        scores: true,
-      },
-    });
-    console.log(games);
-    console.log(userId);
-  }
-  return (
-    <section>
-      <h2>This is the games page</h2>
-      <Suspense fallback={<div>Loading...</div>}>
-        <pre>{JSON.stringify(games, null, 2)}</pre>
 
-        <Table>
-          <TableCaption>A list of games</TableCaption>
-          <TableHeader>
-            <TableRow>
-              <TableHead className="w-[100px]">ID</TableHead>
-              <TableHead>Date</TableHead>
-              <TableHead>Status</TableHead>
-              <TableHead>Players</TableHead>
-              <TableHead className="text-right">Winner</TableHead>
+  if (!userId) {
+    return (
+      <div>
+        <h2>You must be logged in to view this page.</h2>
+      </div>
+    );
+  }
+
+  const games = await prisma.game.findMany({
+    where: {
+      players: {
+        some: {
+          user: {
+            clerk_user_id: userId,
+          },
+        },
+      },
+    },
+    include: {
+      players: {
+        include: {
+          user: true,
+        },
+      },
+      scores: true,
+    },
+  });
+
+  return (
+    <section className="mx-40 my-20">
+      <Button>
+        <Link href="/app/games/new">New game</Link>
+      </Button>
+      <Table>
+        <TableHeader>
+          <TableHead>Game ID</TableHead>
+          <TableHead>Created At</TableHead>
+          <TableHead>Ended At</TableHead>
+          <TableHead>Players</TableHead>
+          <TableHead>Scores</TableHead>
+        </TableHeader>
+        <TableBody>
+          {games.map((game) => (
+            <TableRow key={game.id}>
+              <TableCell>{game.id}</TableCell>
+              <TableCell>{new Date(game.createdAt).toLocaleString()}</TableCell>
+              <TableCell>
+                {game.endedAt
+                  ? new Date(game.endedAt).toLocaleString()
+                  : "Ongoing"}
+              </TableCell>
+              <TableCell>
+                {game.players.map((player) => player.user.email).join(", ")}
+              </TableCell>
+              <TableCell>
+                {game.scores
+                  .map(
+                    (score) =>
+                      `Player: ${score.userId}, Score: ${
+                        score.totalCardsPlayed - score.blitzPileRemaining
+                      }`
+                  )
+                  .join(", ")}
+              </TableCell>
             </TableRow>
-          </TableHeader>
-          <TableBody>
-            {games &&
-              games.map((game) => (
-                <TableRow key={game.id}>
-                  <TableCell>{game.id}</TableCell>
-                  <TableCell>{game.createdAt.toString()}</TableCell>
-                  <TableCell>
-                    {game.isFinished ? "Complete" : "In Progress"}
-                  </TableCell>
-                  <TableCell>
-                    {game.players.map((player) => player.user.email).join(", ")}
-                  </TableCell>
-                  <TableCell className="text-right">
-                    {game.winner ? game.winner.email : "None"}{" "}
-                  </TableCell>
-                </TableRow>
-              ))}
-            <TableRow>
-              <TableCell></TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
-      </Suspense>
+          ))}
+        </TableBody>
+      </Table>
     </section>
   );
 }
--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -1,4 +1,4 @@
-import prisma from "../../../db";
+import prisma from "@/db";
 
 export default async function AppHome() {
   const games = await prisma.game.findMany();
