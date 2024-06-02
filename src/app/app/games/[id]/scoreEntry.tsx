337cd616c4818f81829d049cd7149626ecf0e256
Michael Wickett <mwickett@hashicorp.com>
2024-06-02 19:56:34 -0400

    Improve score entry validation

--- src/app/app/games/[id]/scoreEntry.tsx
+++ src/app/app/games/[id]/scoreEntry.tsx
@@ -6,7 +6,7 @@ import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 
-import createScoresForGame from "./newScoreAction";
+import createScoresForGame from "./createScoresForGameAction";
 
 import { GameWithPlayersAndScores } from "./page";
 import { Player } from "./page";
@@ -25,37 +25,75 @@ export default function ScoreEntry({
     }))
   );
 
+  const [scoresValid, setScoresValid] = useState(false);
+
+  // TODO: Abstract this into an input component
   const handleBlitzPileChange = (userId: string, value: number) => {
-    setPlayerScores((prevScores: Player[]) =>
-      prevScores.map((player) =>
-        player.userId === userId
-          ? { ...player, blitzPileRemaining: value }
-          : player
-      )
-    );
+    const min = 0;
+    const max = 10;
+    if (Number(value) >= min && Number(value) <= max) {
+      setPlayerScores((prevScores: Player[]) =>
+        prevScores.map((player) =>
+          player.userId === userId
+            ? { ...player, blitzPileRemaining: value }
+            : player
+        )
+      );
+    }
+    validateScores();
   };
 
   const handleTotalCardsChange = (userId: string, value: number) => {
-    setPlayerScores((prevScores: Player[]) =>
-      prevScores.map((player) =>
-        player.userId === userId
-          ? { ...player, totalCardsPlayed: value }
-          : player
-      )
-    );
+    const min = 0;
+    const max = 40;
+    if (Number(value) >= min && Number(value) <= max) {
+      setPlayerScores((prevScores: Player[]) =>
+        prevScores.map((player) =>
+          player.userId === userId
+            ? { ...player, totalCardsPlayed: value }
+            : player
+        )
+      );
+    }
+    validateScores();
   };
 
   const handleSubmit = async () => {
     await createScoresForGame(game.id, playerScores);
+    // reset form state
+    setPlayerScores((prevScores: Player[]) =>
+      prevScores.map((player) => ({
+        ...player,
+        blitzPileRemaining: 0,
+        totalCardsPlayed: 0,
+      }))
+    );
+    setScoresValid(false);
+  };
+
+  const validateScores = () => {
+    const isValid = playerScores.every(
+      (player) =>
+        player.blitzPileRemaining >= 0 &&
+        player.blitzPileRemaining <= 10 &&
+        player.totalCardsPlayed >= 0 &&
+        player.totalCardsPlayed <= 40
+    );
+
+    if (isValid) {
+      setScoresValid(true);
+    } else {
+      setScoresValid(false);
+    }
   };
 
   return (
-    <div className="bg-white dark:bg-gray-950 rounded-lg shadow-lg p-6 max-w-md mx-auto">
+    <form className="bg-white dark:bg-gray-950 rounded-lg shadow-lg p-6 max-w-md mx-auto">
       <h1 className="text-2xl font-bold mb-6">Scores</h1>
       <div className="grid gap-4">
         <div className="grid grid-cols-[1fr_1fr_1fr] items-center gap-2 bg-slate-50">
           <p>Player</p>
-          <p>Blitz Pile Left</p>
+          <p>Blitz pile left</p>
           <p>Cards played</p>
         </div>
         {playerScores.map((playerScore: Player) => (
@@ -66,7 +104,7 @@ export default function ScoreEntry({
             <Label htmlFor={playerScore.userId}>{playerScore.email}</Label>
             <Input
               id={playerScore.userId}
-              placeholder="Blitz Cards"
+              placeholder="Blitz cards left"
               type="number"
               value={playerScore.blitzPileRemaining}
               onChange={(e) =>
@@ -76,8 +114,10 @@ export default function ScoreEntry({
                 )
               }
               min={0}
-              max={30}
+              max={10}
               required
+              inputMode="numeric"
+              pattern="[0-9]*"
             />
             <Input
               id="player1-cards"
@@ -91,17 +131,19 @@ export default function ScoreEntry({
                 )
               }
               min={0}
-              max={30}
+              max={40}
               required
+              inputMode="numeric"
+              pattern="[0-9]*"
             />
           </div>
         ))}
       </div>
       <div className="mt-6 flex justify-end">
-        <Button type="submit" onClick={handleSubmit}>
+        <Button type="submit" onClick={handleSubmit} disabled={!scoresValid}>
           Save Scores
         </Button>
       </div>
-    </div>
+    </form>
   );
 }
