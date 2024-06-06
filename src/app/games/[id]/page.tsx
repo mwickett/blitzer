63c6f52967a90320981be78f51480bd41b8efab9
Mike Wickett <mike@wickett.ca>
2024-06-06 17:46:27 -0400

    handle more scoring cases and set winner (#28)

--- src/app/games/[id]/page.tsx
+++ src/app/games/[id]/page.tsx
@@ -2,6 +2,7 @@ import ScoreEntry from "./scoreEntry";
 import ScoreDisplay from "./scoreDisplay";
 import { Game, User, Score } from "@prisma/client";
 import { getGameById } from "@/server/queries";
+import { updateGameAsFinished } from "@/server/mutations";
 
 export interface GameWithPlayersAndScores extends Game {
   players: { user: User; gameId: string; userId: string }[];
@@ -20,6 +21,8 @@ export interface DisplayScores {
   email: string;
   scoresByRound: number[][];
   total: number;
+  isInLead?: boolean;
+  isWinner?: boolean;
 }
 
 export default async function GameView({ params }: { params: { id: string } }) {
@@ -27,19 +30,25 @@ export default async function GameView({ params }: { params: { id: string } }) {
   if (!game) return <div>Game not found</div>;
   // TODO: Throw an error if the game is not found
 
-  // TODO: Handle scoring logic
-
-  const displayScores = transformGameData(game);
+  const displayScores = await transformGameData(game);
 
   return (
     <section>
       <ScoreDisplay displayScores={displayScores} />
-      <ScoreEntry game={game} />
+      {game.isFinished ? (
+        <h2 className="flex justify-center">Game over</h2>
+      ) : (
+        <ScoreEntry game={game} />
+      )}
     </section>
   );
 }
 
-const transformGameData = (game: GameWithPlayersAndScores): DisplayScores[] => {
+// This function is doing _a lot of work_ 😬
+// needs some refactoring / cleanup
+const transformGameData = async (
+  game: GameWithPlayersAndScores
+): Promise<DisplayScores[]> => {
   const userScoresMap: {
     [key: string]: {
       email: string;
@@ -60,6 +69,10 @@ const transformGameData = (game: GameWithPlayersAndScores): DisplayScores[] => {
     };
   });
 
+  let maxScore = -Infinity;
+  let leaders: string[] = [];
+  const playersAboveThreshold: { userId: string; total: number }[] = [];
+
   // Populate the scores by round and calculate the total
   game.scores.forEach((score, index) => {
     const userScore = userScoresMap[score.userId];
@@ -75,8 +88,43 @@ const transformGameData = (game: GameWithPlayersAndScores): DisplayScores[] => {
 
       userScore.scoresByRound[roundIndex].push(scoreValue);
       userScore.total += scoreValue;
+
+      // Update the current leaders
+      if (userScore.total > maxScore) {
+        maxScore = userScore.total;
+        leaders = [score.userId];
+      } else if (userScore.total === maxScore) {
+        leaders.push(score.userId);
+      }
+
+      // Check if the player is above the threshold
+      if (userScore.total >= 75) {
+        playersAboveThreshold.push({
+          userId: score.userId,
+          total: userScore.total,
+        });
+      }
     }
   });
+
+  // Determine the winner if there are players above the threshold
+  let winnerId = null;
+  if (playersAboveThreshold.length > 0) {
+    const highestScore = Math.max(
+      ...playersAboveThreshold.map((player) => player.total)
+    );
+    const potentialWinners = playersAboveThreshold.filter(
+      (player) => player.total === highestScore
+    );
+    if (potentialWinners.length === 1) {
+      winnerId = potentialWinners[0].userId;
+    } else {
+      winnerId = potentialWinners[0].userId; // i need to check the rules on ties
+    }
+    if (!game.isFinished) {
+      await updateGameAsFinished(game.id, winnerId);
+    }
+  }
 
   // Transform the map into the output array
   return Object.entries(userScoresMap).map(
@@ -85,6 +133,8 @@ const transformGameData = (game: GameWithPlayersAndScores): DisplayScores[] => {
       email,
       scoresByRound,
       total,
+      isInLead: leaders.includes(userId),
+      isWinner: userId === winnerId,
     })
   );
 };
