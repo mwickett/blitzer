a5b2f754dc1d07d996326de1b7266790151249d7
Michael Wickett <mwickett@hashicorp.com>
2024-06-10 15:54:59 -0400

    fix ph endpoint

--- src/app/posthog.ts
+++ src/app/posthog.ts
@@ -1,14 +1,24 @@
 // app/posthog.js
 import { PostHog } from "posthog-node";
 
+const host = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}/ingest`
+  : `http://localhost:3000/ingest`;
+
 export default function PostHogClient() {
   const posthogClient = new PostHog(
     process.env.NEXT_PUBLIC_POSTHOG_KEY as string,
     {
-      host: "/ingest",
+      host: host,
       flushAt: 1,
       flushInterval: 0,
     }
   );
+
+  // posthogClient.on("error", (error) => {
+  //   console.error(error);
+  // });
+
+  // posthogClient.debug();
   return posthogClient;
 }
