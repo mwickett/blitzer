b58ac437c4d8ac73c635a1478c677f671cb6f5df
Mike Wickett <mike@wickett.ca>
2024-06-17 09:56:14 -0400

    tidy up stuff

--- src/app/games/new/page.tsx
+++ src/app/games/new/page.tsx
@@ -1,8 +1,18 @@
 import NewGameChooser from "./newGameChooser";
-import { getFriends, getFriendsForNewGame } from "@/server/queries";
+import { getFriendsForNewGame } from "@/server/queries";
+import ClientLink from "@/components/helpers/ClientLink";
 
 export default async function NewGame() {
   const users = await getFriendsForNewGame();
+
+  if (users.length === 0) {
+    return (
+      <div className="flex flex-col items-center h-screen gap-4 mt-4">
+        <p>No friends yet</p>
+        <ClientLink href="/friends/add" label="Add friends" />
+      </div>
+    );
+  }
 
   return <NewGameChooser users={users} />;
 }
