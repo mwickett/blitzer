aeb96ca4ce4761df7fd7a7a78ae953b8be536b25
Mike Wickett <mike@wickett.ca>
2024-07-07 13:08:16 -0400

    abstract user id lookup (#71)

---
+++ src/server/utils.ts
@@ -0,0 +1,24 @@
+import { auth } from "@clerk/nextjs/server";
+import prisma from "@/server/db/db";
+
+// Helper function to get the user's ID from the auth object
+// and then look up the user from the database to retreive 
+
+export async function getUserIdFromAuth() {
+  const user = auth();
+
+  if (!user.userId) throw new Error("Unauthorized");
+
+  const prismaId = await prisma.user.findUnique({
+    where: {
+      clerk_user_id: user.userId,
+    },
+    select: {
+      id: true,
+    },
+  });
+
+  if (!prismaId) throw new Error("User not found");
+
+  return prismaId.id;
+}
