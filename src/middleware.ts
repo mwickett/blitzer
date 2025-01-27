db7e75701492be05d3711765027d586881fcd170
Mike Wickett <mikewickett@clerk.dev>
2025-01-27 09:58:07 -0700

    Refactor navigation and authentication: replace SignUpButton with SignInButton, remove Stats page, and add SignIn page. Update middleware to include public route handling.

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,10 +8,17 @@ const isProtectedRoute = createRouteMatcher([
   "/stats",
 ]);
 
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)'])
+
 export default clerkMiddleware(async (auth, req) => {
   if (isProtectedRoute(req)) await auth.protect();
 });
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
-};
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
+}
