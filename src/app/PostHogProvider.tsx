80d1efc646c6004c78367aeed946de53e1d07a16
Mike Wickett <mike@wickett.ca>
2025-04-05 11:49:56 -0400

    mw.posthog improvements (#146)
    * only create person profiles for ID users
    
    * restore previous PH useEffect deps

--- src/app/PostHogProvider.tsx
+++ src/app/PostHogProvider.tsx
@@ -6,7 +6,6 @@ if (typeof window !== "undefined") {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
     api_host: "/ingest",
     ui_host: "https://us.posthog.com",
-    person_profiles: "always", // or 'always' to create profiles for anonymous users as well
     capture_pageview: false,
     capture_pageleave: true,
   });
